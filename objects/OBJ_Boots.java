fe0f8a317a0a69eed1b12eff49f254f2b6588525
gibletchaser <hazzziq412@gmail.com>
2025-10-08 00:23:59 +0800

    Update OBJ_Boots.java

--- objects/OBJ_Boots.java
+++ objects/OBJ_Boots.java
@@ -1,26 +1,16 @@
 package objects;
 
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
+import Entity.Entity;
 import main.GamePanel;
 
-public class OBJ_Boots extends SuperObject{
+public class OBJ_Boots extends Entity{
 
-        GamePanel gp;
 
     public OBJ_Boots(GamePanel gp){
-        this.gp = gp;
+
+        super(gp);
 
         name = "Boots";
-
-        try{
-            image = ImageIO.read(getClass().getResourceAsStream("/object/boots.png"));
-            uTool.scaleImage(image, gp.tileSize, gp.tileSize);
-
-        }catch(IOException e){
-            e.printStackTrace();
-        }
+        down1 = setup("/object/boots");
     }
 }
